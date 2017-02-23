02ddf40c852c145bae0406e9ca387fb4bef9d5a5
schwarzm3283 <schwarz.mike@gmail.com>
2017-02-23 10:22:29 -0500

    started diamond examples

--- book_examples.R
+++ book_examples.R
@@ -1,1 +1,20 @@
+
+#   ____________________________________________________________________________
+#   Load Librarys                                                           ####
 library(nycflights13)
+library(tidyverse)
+
+##  ............................................................................
+##  Diamond examples                                                        ####
+smaller <- diamonds %>% 
+  filter(carat < 3)
+
+  #histogram
+ggplot(data = smaller, mapping = aes(x = carat)) +
+  geom_histogram(binwidth = 0.1)
+
+  #freqpoly
+ggplot(data = smaller, mapping = aes(x = carat, color = cut)) +
+  geom_freqpoly(binwidth = 0.1)
+
+
